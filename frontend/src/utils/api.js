d67a0159b2d17064f8c5b6357d64e97154760be8
Abhishek Mandal <abhishekabk1997@gmail.com>
2025-11-11 09:55:05 +0530

    Updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API = process.env.REACT_APP_API || "http://localhost:3000"; // change to 5000 if backend uses 5000
+const API = process.env.REACT_APP_API || "https://crm-project-1-khrc.onrender.com"; 
 
 async function request(path, method="GET", body=null, token=null){
   const headers = { "Content-Type":"application/json" };
